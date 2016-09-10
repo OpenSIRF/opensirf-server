abd4d5bf944a57e44e12a5ad71a52c092cd8085f
Phillip Viana <pviana@us.ibm.com>
2016-09-10 14:25:15 -0500

    Fixing wrong Copyright information.

--- test/main/java/org/opensirf/jaxrs/api/SwiftStrategyTest.java
+++ test/main/java/org/opensirf/jaxrs/api/SwiftStrategyTest.java
@@ -1,13 +1,33 @@
 /*
- * IBM Confidential
- *
- * OCO Source Materials
- *
- * (C) Copyright IBM Corp. 2016
- *
- * The source code for this program is not published or other-
- * wise divested of its trade secrets, irrespective of what has
- * been deposited with the U.S. Copyright Office.
+ * OpenSIRF JAX-RS
+ * 
+ * Copyright IBM Corporation 2015.
+ * All Rights Reserved.
+ * 
+ * MIT License:
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * 
+ * Except as contained in this notice, the name of a copyright holder shall not
+ * be used in advertising or otherwise to promote the sale, use or other
+ * dealings in this Software without prior written authorization of the
+ * copyright holder.
  */
 
 package org.opensirf.jaxrs.api;
