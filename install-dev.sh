631f7e7d7314760470dbb66798630bad0668c734
Phillip Viana <phillip.l.viana@gmail.com>
2017-01-21 09:49:26 -0600

    Adding calls to Chef recipe from install script.

--- install-dev.sh
+++ install-dev.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # Constants
+args=$1
+gitBranch="develop"
 swiftBoxName="devsirfswift"
 fsBoxName="devsirffs"
 sirfBoxName="devsirfserver"
+chefSetup="https://raw.githubusercontent.com/OpenSIRF/opensirf-server/$gitBranch/chef-setup.sh"
 
 # $1=numErrors
 errorExit() {
@@ -31,89 +34,104 @@ checkPreReqs() {
 }
 
 prepare() {
-  mkdir sirf-dev
-  mkdir sirf-dev/opensirf-server
-  mkdir sirf-dev/opensirf-swift
-  mkdir sirf-dev/opensirf-fs
+  if [ "$1" != "-n" ]; then
+      mkdir -p sirf-dev
+      mkdir -p sirf-dev/$sirfBoxName
+      mkdir -p sirf-dev/$swiftBoxName
+      mkdir -p sirf-dev/$fsBoxName
 
-  cat <<-EOF > sirf-dev/opensirf-server/Vagrantfile
-    Vagrant.configure("2") do |config|
-      config.vm.box = "opensirf/opensirf-server"
-      config.vm.hostname = "$sirfBoxName"
-      config.ssh.insert_key = false
-      config.vm.provider "virtualbox" do |v|
-	v.name = "$sirfBoxName"
-        config.vm.network "public_network", :adapter => 3
-        config.vm.network "private_network", :type => 'dhcp', :name => 'vboxnet0', :adapter => 2
-      end
-    end
+      cat <<-EOF > sirf-dev/$sirfBoxName/Vagrantfile
+        Vagrant.configure("2") do |config|
+          config.vm.box = "opensirf/opensirf-server"
+          config.vm.hostname = "$sirfBoxName"
+          config.ssh.insert_key = false
+          config.vm.provider "virtualbox" do |v|
+            v.name = "$sirfBoxName"
+            config.vm.network "public_network", :adapter => 3
+            config.vm.network "private_network", :type => 'dhcp', :name => 'vboxnet0', :adapter => 2
+          end
+        end
 	EOF
 
-  cat <<-EOF > sirf-dev/opensirf-swift/Vagrantfile
-	Vagrant.configure("2") do |config|
-			config.vm.box = "opensirf/opensirf-ident-swift"
-			config.vm.hostname = "$swiftBoxName"
-			config.ssh.insert_key = false
-			config.vm.provider "virtualbox" do |v|
-				v.memory = 4096
-				v.cpus = 4
-				v.name = "$swiftBoxName"
-				config.vm.network "private_network", :type => 'dhcp', :name => 'vboxnet0', :adapter => 2
-				config.vm.network "public_network", :adapter => 3
-			end
-		end
+      cat <<-EOF > sirf-dev/$swiftBoxName/Vagrantfile
+      Vagrant.configure("2") do |config|
+          config.vm.box = "opensirf/opensirf-ident-swift"
+          config.vm.hostname = "$swiftBoxName"
+          config.ssh.insert_key = false
+          config.vm.provider "virtualbox" do |v|
+            v.memory = 4096
+            v.cpus = 4
+            v.name = "$swiftBoxName"
+            config.vm.network "private_network", :type => 'dhcp', :name => 'vboxnet0', :adapter => 2
+            config.vm.network "public_network", :adapter => 3
+          end
+        end
 	EOF
 
-  cat <<-EOF > sirf-dev/opensirf-fs/Vagrantfile
-		Vagrant.configure("2") do |config|
-			config.vm.box = "opensirf/opensirf-base"
-			config.vm.hostname = "$fsBoxName"
-			config.ssh.insert_key = false
-			config.vm.provider "virtualbox" do |v|
-				v.name = "$fsBoxName"
-				config.vm.network "public_network", :adapter => 3
-				config.vm.network "private_network", :type => 'dhcp', :name => 'vboxnet0', :adapter => 2
-			end
-		end
+      cat <<-EOF > sirf-dev/$fsBoxName/Vagrantfile
+        Vagrant.configure("2") do |config|
+          config.vm.box = "opensirf/opensirf-base"
+          config.vm.hostname = "$fsBoxName"
+          config.ssh.insert_key = false
+          config.vm.provider "virtualbox" do |v|
+            v.name = "$fsBoxName"
+            config.vm.network "public_network", :adapter => 3
+            config.vm.network "private_network", :type => 'dhcp', :name => 'vboxnet0', :adapter => 2
+          end
+        end
 	EOF
-
+  fi
 }
 
 # $1=box name
-resolveHost() {
-  #boxId=$(cat .vagrant/machines/default/virtualbox/index_uuid)
+addHost() {
   pubIP=$(VBoxManage guestproperty get $1 "/VirtualBox/GuestInfo/Net/2/V4/IP" | awk '{print $2}')
-  echo "$pubIP $1" >> ../hosts.add
+  ip[$1]=$pubIP
+}
+
+# $1=box name
+provisionServer() {
+  oldDir=$(pwd)
+  cd sirf-dev/$1
+  if [ "$args" != "-n" ]; then
+    vagrant up
+  fi
+  vmId[$1]=$(cat .vagrant/machines/default/virtualbox/index_uuid)
+  addHost $1
+  cd $oldDir
+  runChefSetup $1
 }
 
 provisionSirfServer() {
-  oldDir=$(pwd)
-  cd sirf-dev/opensirf-server
-  vagrant up
-  resolveHost $sirfBoxName
-  cd $oldDir
+  provisionServer $sirfBoxName
 }
 
 provisionSwiftServer() {
-  oldDir=$(pwd)
-  cd sirf-dev/opensirf-swift
-  pwd
-  ls *
-  vagrant up
-  resolveHost $swiftBoxName
-  cd $oldDir
+  provisionServer $swiftBoxName
 }
 
 provisionFsServer() {
-  oldDir=$(pwd)
-  cd sirf-dev/opensirf-fs
-  vagrant up
-  resolveHost $fsBoxName
-  cd $oldDir
+  provisionServer $fsBoxName
 }
+
+runChefSetup() {
+  vagrant ssh ${vmId[$1]} -c "sudo sh -c \"curl -s $chefSetup | bash\""
+}
+
+configureHosts() {
+  for box in $sirfBoxName $swiftBoxName $fsBoxName; do
+    for host in $sirfBoxName $swiftBoxName $fsBoxName; do
+      vagrant ssh ${vmId[$box]} -c "sudo sh -c \"echo ${ip[$host]} $host >> /etc/hosts\""
+    done
+  done
+}
+
+declare -A ip
+declare -A vmId
 
 checkPreReqs
 prepare
 provisionSirfServer
 provisionSwiftServer
 provisionFsServer
+configureHosts
